9c3828f3c21d1a8235b659973fce025f487c483e
pvrvd0x <d3m35s1ah@protonmail.com>
2021-11-06 03:33:07 +0300

    Implement footer + animations + link scrolling

---
+++ src/js/scroll.js
@@ -0,0 +1,10 @@
+// ;(function() {
+//     document.querySelectorAll('.link--scroll').forEach(link => {
+//         link.addEventListener('click', (e) => {
+//             e.preventDefault();
+//             document.getElementById(e.target.dataset.id).scrollIntoView({
+//                 'behavior': 'smooth'
+//             });
+//         });
+//     })
+// })();
